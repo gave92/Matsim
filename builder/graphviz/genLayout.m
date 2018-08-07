afe218b31be920988a7968e6432aa55b26fed13b
Marco Gavelli <marcogavelli2@gmail.com>
2018-08-07 23:48:09 +0200

    Fixed calling GraphViz

--- builder/graphviz/genLayout.m
+++ builder/graphviz/genLayout.m
@@ -67,8 +67,7 @@ end
 function callGraphViz(obj)
 % Call GraphViz to determine an optimal layout. Write this layout in
 % layout.dot for later parsing. 
-   graphviz_path = getenv('GRAPHVIZ');
-   err = system([fullfile(graphviz_path, 'dot'),' -Tdot -Gmaxiter=5000 -Gstart=7 -o ',obj.layoutFile,' ',obj.adjFile]);
+   err = system(['dot -Tdot -Gmaxiter=5000 -Gstart=7 -o ',obj.layoutFile,' ',obj.adjFile]);
    if(err),error('Sorry, unknown GraphViz failure, try another layout'); end
 end
 
