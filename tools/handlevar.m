bb3cedafb9a29f48b4f367c5a048a772820475f3
Marco Gavelli <marcogavelli2@gmail.com>
2019-05-25 14:41:33 +0200

    Added missing handlevar.m for generator

---
+++ tools/handlevar.m
@@ -0,0 +1,12 @@
+classdef handlevar < handle
+    properties
+        Value
+    end
+    
+    methods
+        function this = handlevar(value)
+            this.Value = value;
+        end
+    end
+    
+end
